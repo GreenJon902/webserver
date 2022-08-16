99d5d1edc01b846022330f10f2ad37463bff52c7
greenjon902 <greenjon902@gmail.com>
2022-08-16 12:15:48 +0100

    Initial commit

---
+++ src/main/java/com/greenjon902/webserver/Webserver.java
@@ -0,0 +1,17 @@
+package com.greenjon902.webserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Webserver extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
